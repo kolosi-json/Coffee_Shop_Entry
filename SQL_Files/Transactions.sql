4abc13ce538b3d4569bb86d40df948eb031806bb
Kolosi <laurequezian03@gmail.com>
2026-08-19 14:28:51 +0800

    Update Transactions.sql

--- SQL_Files/Transactions.sql
+++ SQL_Files/Transactions.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Transactions (
-	  transaction_id INT AUTO_INCREMENT NOT NULL,
-  	customer_id INT,
-  	item_id INT,
+	  	transaction_id INT AUTO_INCREMENT NOT NULL,
+  		customer_id INT,
+  		item_id INT,
   
  	  CONSTRAINT fk_customer
   	FOREIGN KEY (customer_id)
@@ -9,4 +9,13 @@ CREATE TABLE Transactions (
   
 	  CONSTRAINT fk_item
   	FOREIGN KEY (item_id)
-  	REFERENCES Menu(item_id);
+  	REFERENCES Menu(item_id)
+	);
+
+--Code For the Total Amount of Purchase Each Item--
+
+SELECT Menu.item_name, COUNT(Menu.item_name) AS 'Number of Items Bought'
+FROM Transactions
+INNER JOIN Menu ON Menu.item_id = Transactions.item_id
+GROUP BY Menu.item_name
+ORDER BY Menu.item_name DESC
